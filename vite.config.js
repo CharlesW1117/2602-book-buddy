37ae7fabdedaca182a1799f3f47fc6e27d69c1e7
Charles Wade <Charleskwadejr1088@gmail.com>
2026-06-16 11:56:51 -0500

    corrections to make the project live.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/2602-book-buddy/", // 👈 must match your repo name exactly
+  base: '/2602-book-buddy/', // 👈 must match your repo name exactly
 });
